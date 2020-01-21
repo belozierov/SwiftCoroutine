0fdbaf15565b474e1f9afb6dd008af44b79f33cb
Alex Belozierov <oleksandr.belozierov@kindgeek.com>
2020-01-21 23:34:45 +0200

    - add AnyCoCancellable

--- Sources/SwiftCoroutine/Protocols/CoCancellable.swift
+++ Sources/SwiftCoroutine/Protocols/CoCancellable.swift
@@ -11,5 +11,41 @@ import Combine
 public protocol CoCancellable: Cancellable, Hashable {
     
     func cancel()
+    func cancelUpstream()
+    
+}
+
+extension CoCancellable where Self: AnyObject {
+    
+    @inlinable var anyCoCancellable: AnyCoCancellable {
+        AnyCoCancellable(self)
+    }
+    
+}
+
+final public class AnyCoCancellable: CoCancellable {
+    
+    private let _cancel, _cancelUpstream: () -> Void
+    
+    public init<T: CoCancellable & AnyObject>(_ cancellable: T) {
+        _cancel = { [weak cancellable] in cancellable?.cancel() }
+        _cancelUpstream = { [weak cancellable] in cancellable?.cancelUpstream() }
+    }
+    
+    public func cancel() {
+        _cancel()
+    }
+    
+    public func cancelUpstream() {
+        _cancelUpstream()
+    }
+    
+    @inlinable public static func == (lhs: AnyCoCancellable, rhs: AnyCoCancellable) -> Bool {
+        lhs === rhs
+    }
+    
+    @inlinable public func hash(into hasher: inout Hasher) {
+        ObjectIdentifier(self).hash(into: &hasher)
+    }
     
 }
