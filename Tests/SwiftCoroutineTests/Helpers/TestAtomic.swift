e8e200c9af33874b45724feb981c5361c084410f
Alex Belozierov <oleksandr.belozierov@kindgeek.com>
2020-04-26 13:34:25 +0300

    - updated tests

--- Tests/SwiftCoroutineTests/Helpers/TestAtomic.swift
+++ Tests/SwiftCoroutineTests/Helpers/TestAtomic.swift
@@ -10,77 +10,7 @@ import XCTest
 import Dispatch
 @testable import SwiftCoroutine
 
-struct AtomicList<T> {
-    
-    private struct Node {
-        let value: T
-        var next = 0
-    }
-    
-    private var head = AtomicInt()
-    
-    mutating func push(_ value: T) {
-        let pointer = UnsafeMutablePointer<Node>.allocate(capacity: 1)
-        pointer.initialize(to: Node(value: value))
-        head.update {
-            pointer.pointee.next = $0
-            return Int(bitPattern: pointer)
-        }
-    }
-    
-    var count: Int {
-        var count = 0
-        for _ in self { count += 1 }
-        return count
-    }
-    
-}
-
-extension AtomicList: Sequence {
-    
-    func makeIterator() -> AnyIterator<T> {
-        var address = head.value
-        return AnyIterator {
-            guard address > 0, let pointer = UnsafePointer<Node>(bitPattern: address)
-                else { return nil }
-            address = pointer.pointee.next
-            return pointer.pointee.value
-        }
-    }
-    
-}
-
-struct ThreadSafeList<T> {
-    
-    private let mutex = PsxLock()
-    private var array = [T]()
-    
-    mutating func push(_ value: T) {
-        mutex.lock()
-        array.append(value)
-        mutex.unlock()
-    }
-    
-    var count: Int {
-        array.count
-    }
-    
-}
-
 class TestAtomic: XCTestCase {
-    
-    func testAbc() {
-        var list = AtomicList<Int>()
-        measure {
-//            for i in 0..<100_000 {
-//                list.push(i)
-//            }
-            DispatchQueue.concurrentPerform(iterations: 100_000) {
-                list.push($0)
-            }
-        }
-        print(list.count)
-    }
     
     func testTuple() {
         var tuple = AtomicTuple()
